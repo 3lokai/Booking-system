f7ceae8ca35326c78a012ee7250f9ea4904004ca
GT <gta3lok.ai@gmail.com>
2024-12-17 19:36:54 +0530

    updated next.config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,  // This will ignore ESLint errors during build
-  }
-};
-
-export default nextConfig;
